acbb43963c86cfb0bb059074e90dfc711bf53b7d
Sam Mangham <mangham@gmail.com>
2023-12-01 14:30:07 +0000

    Updated setup.py and pyproject

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='test',
     version='0.1.0',
     packages=find_packages(include=['test', 'test.*'])
     install_requires=[
-        'numpy>=1.14.5',
+        'numpy==1.20.0',
     ]
 )
 
